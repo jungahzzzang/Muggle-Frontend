92091cebbd876f70648fd20d34374c719df13214
Yeom Jung Ah <jungahzzzang@gmail.com>
2022-11-14 14:56:26 +0900

    build:expo 환경 삭제

--- FrontEnd/src/screens/Index.js
+++
@@ -1,7 +0,0 @@
-import Login from "./Login"
-import StudyChat from "./StudyChat";
-import ChatCreation from "./ChatCreation";
-import StudyChatList from "./StudyChatList";
-import Mypage from "./Mypage";
-
-export {Login, StudyChat, ChatCreation, StudyChatList, Mypage };
