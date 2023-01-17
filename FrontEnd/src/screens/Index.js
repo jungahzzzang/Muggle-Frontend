4f52da881ae1b7a44f3e3a0564744e579c0f016c
jungahzzzang <jungahzzzang@gmail.com>
2023-01-17 22:52:01 +0900

    fix:splash screen 넘어가지 않는 문제

--- FrontEnd/src/screens/Index.js
+++
@@ -1,4 +0,0 @@
-import FAQScreen from "./mypage/FAQScreen";
-import NoticeListScreen from "./mypage/NoticeListScreen";
-import PerrsonalInfo from "./mypage/PersonalInfo";
-import ProfileScreen from "./mypage/ProfileScreen";
