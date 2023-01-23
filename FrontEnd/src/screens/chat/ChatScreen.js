0e7e5770ac93c36dfddb25db87054b67df743cc4
jungahzzzang <jungahzzzang@gmail.com>
2023-01-23 21:41:26 +0900

    feat:굿즈거래소 화면 추가

---
+++ FrontEnd/src/screens/chat/ChatScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const ChatScreen = () => {
+
+    return (
+        <View>
+            <Text>채팅방</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});
+
+export default ChatScreen;
