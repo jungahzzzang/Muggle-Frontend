2d582a829b19bcfb4df10fd59e8c4c23916b3831
itmdeveloper <choidasol95@gmail.com>
2023-04-25 19:59:05 +0900

    백엔드 폴더 삭제

--- BackEnd/demo/src/main/java/com/example/demo/oauth/exception/OAuthProviderMissMatchException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.oauth.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException {
-
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
-
